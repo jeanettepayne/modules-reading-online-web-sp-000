ab138f51f4cdec61450c1d3bb65f7eda9397c574
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-09 22:27:07 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,9 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  extend MetaDancing
   include Dance 
-  
+  extend MetaData
   
   attr_accessor :name 
   
